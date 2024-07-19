e871ec2c6d84c7cd5ec6ec27aa2435943b1a3f58
zw427 <168679060+zw427@users.noreply.github.com>
2024-07-19 15:30:46 -0400

    Add files via upload

--- HPC-ED_Search.py
+++ HPC-ED_Search.py
@@ -1,11 +1,12 @@
 import argparse
 import copy
+import distutils
 import globus_sdk
 import os
 import pandas as pd
 import subprocess
 import sys
-import time
+import datetime
 import textwrap
 
 
@@ -15,13 +16,13 @@ INDEX_ID = ""
 ###################################################
 
 
-def process_title(title):
+def html_title(title):
     if title:
-        html = f'<b>{title}</b>'
-        return html
+        title = f'<b>{title}</b>'
+        return title
     return ''
 
-def process_authors(authors):
+def html_authors(authors):
     if authors is None:
         return ''
     if isinstance(authors, list):
@@ -29,13 +30,13 @@ def process_authors(authors):
     else:
         return authors
 
-def process_url(url):
+def html_url(url):
     if url:
-        html = f'<a href="{url}" target="_blank" rel="noopener noreferrer">{url}</a>'
-        return html
+        url = f'<a href="{url}" target="_blank" rel="noopener noreferrer">{url}</a>'
+        return url
     return ''
 
-def process_keywords(keywords):
+def html_keywords(keywords):
     if keywords is None:
         return ''
     if isinstance(keywords, list):
@@ -43,12 +44,12 @@ def process_keywords(keywords):
     else:
         return keywords
 
-def process_abstract(abstract):
+def html_abstract(abstract):
     if abstract:
         return abstract.replace('\\"', '"')
     return ''
 
-def process_duration(duration):
+def html_duration(duration):
     if duration:
         try:
             return str(int(duration))
@@ -56,7 +57,7 @@ def process_duration(duration):
             return ''
     return ''
 
-def process_rating(rating):
+def html_rating(rating):
     if rating:
         try:
             return format(float(rating), ".1f")
@@ -65,6 +66,12 @@ def process_rating(rating):
     return ''
 
 
+def markdown_title(title):
+    if title:
+        title = f'**{title}**'
+        return title
+    return ''
+
 def markdown_link(url):
     if url:
         return "[link](" + url + ")"
@@ -72,16 +79,22 @@ def markdown_link(url):
 
 def markdown_abs(abs):
     if abs:
-        return abs.replace("<br>", " ").replace("</br>", "").replace("\n", "")
+        return abs.replace("<br>", " ").replace("</br>", "").replace("\n", " ").replace("\r", " ")
     return ''
 
+
+def str2bool(v):
+    '''
+    Provides flexible parameter input
+    '''
+    return bool(distutils.util.strtobool(v))
 
 description = textwrap.dedent(
     '''
     Globus Search Simple Script
     --------------------------------------------
      * (PACKAGES) Requires globus-sdk and pandas
-     * Index_ID can be set in this file. 
+     * Index_ID can be set in the file. 
      * One of query, provider, authors, keywords, or title must be provided as parameter. 
      * If you want to include '-' in the query, it needs to be written as '\-'.
      * If --limit and --offset are not provided, show all discovered entries. 
@@ -114,10 +127,7 @@ epilog = textwrap.dedent(
     6) python3 HPC-ED_Search.py --index_id $INDEX_ID --keywords 'python' --outpath python
     Searches for entries with 'python' in the keywords and saves it to 'python.html' in the current working directory.
 
-    7) python3 HPC-ED_Search.py --index_id $INDEX_ID --keywords 'python' --outpath HPC-ED_Docs/python
-    Searches for entries with 'python' in the keywords and saves it to 'python.html' in 'HPC-ED_Docs' directory, if it does not exist, the directory will be made. 
-
-    8) python3 HPC-ED_Search.py --index_id $INDEX_ID --keywords 'python' --outpath ~/HPC-ED_Docs/python
+    7) python3 HPC-ED_Search.py --index_id $INDEX_ID --keywords 'python' --outpath ~/HPC-ED_Docs/python
     Searches for entries with 'python' in the keywords and saves it to 'python.html' in '~/HPC-ED_Docs/' directory, if it does not exist, the directory will be made. 
     '''
 )
@@ -131,15 +141,27 @@ def main():
     parser.add_argument('--authors', help='Authors (optional).')
     parser.add_argument('--keywords', help='Keywords (optional).')
     parser.add_argument('--title', help='Title (optional).')
-    parser.add_argument('--markdown', type=bool, default=False, help='markdown (default false, HTML).')
+
     parser.add_argument('--limit', type=int, help='Limit (optional).')
     parser.add_argument('--offset', type=int, help='Offset (optional).')
-    parser.add_argument('--advanced', type=bool, default=True, help='Advanced search (default true).')
-    parser.add_argument('--outpath', help='Output file path (Optional). If not provided, HTML file name is based on time.')
+    parser.add_argument("--advanced", type=str2bool, nargs='?',
+                        const=True, default=True,
+                        help='Advanced search (default true).')
+
+    parser.add_argument("--markdown", type=str2bool, nargs='?',
+                        const=True, default=False,
+                        help='markdown (default false, output is HTML).')
+    parser.add_argument('--outpath', help='Output file path (Optional). If not provided, file name is based on time.')
 
     args = parser.parse_args()
 
+    # Make a search client
     search_client = globus_sdk.SearchClient()
+
+    try:
+        INDEX_ID
+    except:
+        INDEX_ID = ""
 
     # Check Index ID
     index_id = args.index_id
@@ -160,12 +182,13 @@ def main():
         if args.title: qs.append(f'Title:{args.title}')
         qs = " AND ".join(qs)
     else:
-        print("[Error] None of query, provider, authors, keywords, titles are provided")
+        print("[Error] None of query, provider, authors, keywords, and titles are provided")
         sys.exit(0)
 
     # resolve some issue with -
     qs = qs.replace("\\-", "-")
-    print(f"[INFO] Searching: '{qs}'")
+    qs_info = f"[INFO] Searching query: '{qs}'"
+    print(qs_info)
 
     # number of entries to display
     display_all = args.limit is None and args.offset is None
@@ -173,7 +196,12 @@ def main():
     offset = args.offset if args.offset else 0
     
     # Search
-    response = search_client.search(index_id, qs, limit=limit, offset=offset, advanced=args.advanced)
+    try:
+        response = search_client.search(index_id, qs, limit=limit, offset=offset, advanced=args.advanced)
+    except:
+        print("[Error] Please check your internet connection, check the INDEX_ID, \
+                and check the query string for possible errors.")
+        sys.exit(0)
 
     # if no entries
     if response.data['total'] == 0:
@@ -181,57 +209,79 @@ def main():
         sys.exit(0)
 
     # if entries found
+    try:
+        total = response.data['total']
+        count = response.data['count']
+        response_offset = response.data['offset']
+    except:
+        print("[Error] Globus Search Failed. Please email HPC-ED.")
+        sys.exit(0)
 
-    total = response.data['total']
-    count = response.data['count']
-    offset2 = response.data['offset']
-
+    # show how many are displayed
+    
     if display_all:
-        print(f'[INFO] {total} entries found')
+        display = f'[INFO] {total} entries found'
     else:
-        print(f'[INFO] {total} entries found, displaying {offset2} to {offset2 + count - 1}')
+        display = f'[INFO] {total} entries found, displaying {response_offset} to {response_offset + count - 1}'
+    print(display)
 
+    # copy (save) data
     entries = copy.deepcopy(response.data['gmeta'])
 
     # get more entries
-    if display_all:
-        while response.data['has_next_page']:
-            offset = offset + limit
-            response = search_client.search(index_id, qs, limit=limit, offset=offset, advanced=args.advanced)
-            entries = entries + copy.deepcopy(response.data['gmeta'])
+    try:
+        if display_all:
+            while response.data['has_next_page']:
+                offset = offset + limit
+                response = search_client.search(index_id, qs, limit=limit, offset=offset, advanced=args.advanced)
+                entries = entries + copy.deepcopy(response.data['gmeta'])
+    except:
+        print("[Error] Please check your internet connection, check the INDEX_ID, \
+                and check the query string for possible errors.")
+        sys.exit(0)
 
-    # columns to display, in this order
+    # columns to display in the table, in this order
     columns = ['Title', 'Authors', 'URL', 'Keywords', 'Abstract', 'Duration', 'Rating']
 
     # Get metadata from the entries
-    metadata = []
-    for entry in entries:
-        res = {}
-        for col in columns:
-            if col in entry['entries'][0]['content'].keys():
-                res[col] = entry['entries'][0]['content'][col]
-            else: 
-                res[col] = None
-        metadata.append(res)
+    try:
+        metadata = []
+        for entry in entries:
+            res = {}
+            for col in columns:
+                if col in entry['entries'][0]['content'].keys():
+                    res[col] = entry['entries'][0]['content'][col]
+                else: 
+                    res[col] = None
+            metadata.append(res)
+    except:
+        print("[Error] Globus Search Failed. Please email HPC-ED.")
+        sys.exit(0)
 
-
-    # Outpath
-    outpath = args.outpath if args.outpath else str(time.time())
+    # Set outpath
+    outpath = args.outpath if args.outpath else datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     if args.markdown:
         absolute_outpath = os.path.abspath(outpath + ".md") 
     else:
         absolute_outpath = os.path.abspath(outpath + ".html") 
     os.makedirs(os.path.split(absolute_outpath)[0], exist_ok=True)
-    # Panda Dataframe and to html
 
+    # Panda Dataframe and to file
     if not args.markdown:
+        # Make Pandas Dataframe and to html
         try:
             df = pd.DataFrame(metadata, columns=columns)
-            html = df.to_html(absolute_outpath, escape=False, formatters=[
-                process_title, process_authors, process_url, process_keywords, process_abstract, process_duration, process_rating
+            df.to_html(absolute_outpath, escape=False, formatters=[
+                html_title, html_authors, html_url, html_keywords, html_abstract, html_duration, html_rating
             ])
+
+            # with open(absolute_outpath, "r+") as f:
+            #     s = f.read() 
+            #     f.seek(0)
+            #     f.write(qs_info + "<br>" + display + "<br>" + s)
+
         except:
-            print("[ERROR] Panda Error")
+            print("[ERROR] Pandas Error")
             sys.exit(0)
 
         print(f"[INFO] HTML file: {absolute_outpath}")
@@ -246,16 +296,25 @@ def main():
                 print('[ERROR] Could not open html file')
     else:
         try:
+            # Make Pandas Dataframe and to md
             df = pd.DataFrame(metadata, columns=columns)
-            print(df.iloc[5, 4])
+            df.iloc[:,0] = df.iloc[:,0].apply(markdown_title)
+            df.iloc[:,1] = df.iloc[:,1].apply(html_authors)
             df.iloc[:,2] = df.iloc[:,2].apply(markdown_link)
+            df.iloc[:,3] = df.iloc[:,3].apply(html_keywords)
             df.iloc[:,4] = df.iloc[:,4].apply(markdown_abs)
-            print(df.iloc[5, 4])
-            markdown = df.to_markdown(absolute_outpath)
-            
-            print(f"[INFO] HTML file: {absolute_outpath}")
+            df.iloc[:,5] = df.iloc[:,5].apply(html_duration)
+            df.iloc[:,6] = df.iloc[:,6].apply(html_rating)
+            df.to_markdown(absolute_outpath)
+
+            # with open(absolute_outpath, "r+") as f:
+            #     s = f.read()
+            #     f.seek(0)
+            #     f.write(qs_info + "<br>" + display + "\n" + s)
+
+            print(f"[INFO] Markdown file: {absolute_outpath}")
         except:
-            print("[ERROR] Panda Error")
+            print("[ERROR] Pandas Error")
             sys.exit(0)
 
 
