5161396f977619f718e9f130220fcb00744fb7b9
John-Paul Navarro <navarro@anl.gov>
2024-07-22 10:46:27 -0400

    Fix commands

--- Search_Commands.sh
+++ Search_Commands.sh
@@ -6,7 +6,6 @@ globus search index list
 globus search index show $INDEX_ID
 globus search index show $INDEX_ID -F json
 globus search query $INDEX_ID -q \*
-globus search query $INDEX_ID -q \*
-globus search query $INDEX_ID -q \*
+globus search query $INDEX_ID -q \* -F json
 globus search subject show $INDEX_ID "urn:ogf.org:glue2:access-ci.org:resource:cider:infrastructure.organizations:898:localid_456" 
 
