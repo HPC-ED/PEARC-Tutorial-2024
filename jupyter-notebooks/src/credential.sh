22b3a517413ea5ffad23e002b323da81baf18249
Mary Thomas <marypthomas88@gmail.com>
2024-07-22 14:35:43 -0400

    add tutorial credentials

--- jupyter-notebooks/src/credential.sh
+++ jupyter-notebooks/src/credential.sh
@@ -1,6 +1,6 @@
 #!/user/bin/bash
-
 export INDEX_ID=a2017194-aa8a-41d2-af21-ba8ea79039e5
 export GLOBUS_CLI_CLIENT_ID=9944b935-5878-49cc-8aaa-c996fed60054
 export GLOBUS_CLI_CLIENT_SECRET=kpTCytCwSYrg7ZTgSFIy/kk1UOntC3t2LOpqVp2hk9c=
 export PROVIDER_ID=urn:ogf.org:glue2:access-ci.org:resource:cider:infrastructure.organizations:PEARC24
+
