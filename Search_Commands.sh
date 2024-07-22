59345d681896e348a4a1d7c24a3e3dc4fa6cb784
John-Paul Navarro <navarro@anl.gov>
2024-07-22 11:39:28 -0400

    A query example

--- Search_Commands.sh
+++ Search_Commands.sh
@@ -8,4 +8,4 @@ globus search index show $INDEX_ID -F json
 globus search query $INDEX_ID -q \*
 globus search query $INDEX_ID -q \* -F json
 globus search subject show $INDEX_ID "urn:ogf.org:glue2:access-ci.org:resource:cider:infrastructure.organizations:898:localid_456" 
-
+globus search query $INDEX_ID -q 'Provider_ID:"urn:ogf.org:glue2:access-ci.org:resource:cider:infrastructure.organizations:PEARC24"'
